7dba471d8ef987a658072eb5ad67053a8677b47e
ammar <ammarraneez@gmail.com>
2022-08-26 16:28:35 +0530

    Create lesson type

---
+++ src/lesson/lesson.type.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+
+@ObjectType('Lesson')
+export class LessonType {
+  @Field(type => ID)
+  id: string;
+
+  @Field()
+  name: string;
+
+  @Field()
+  startDate: string;
+
+  @Field()
+  endDate: string;
+}
